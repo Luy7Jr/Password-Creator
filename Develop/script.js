58380a2de12e2d743d1ad4eed1fb3e9a7ac3ee5f
Luy10Jr <luy10jr3@gmail.com>
2021-05-20 16:05:47 -0400

    debbuging 2

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var collection0Valids = [];
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
+var lowerCaseCharacters = "abcdefghiklmnopqrsttuvwxtz" .split("");
+var numberCharacters = "0123456789" .split ("");
+var specialCharacters = "*&^%$#@!?><{}" .split("");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,12 +27,6 @@ function generatePassword () {
         alert("Error! Please Try Again.");
         return;
     }
-
-    var collection0Valids = [];
-    var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
-    var lowerCaseCharacters = "abcdefghiklmnopqrsttuvwxtz" .split("");
-    var numberCharacters = "0123456789" .split ("");
-    var specialCharacters = "*&^%$#@!?><{}" .split("");
 
     var lowerCaseConfirm = confirm("Want your password to contain lowercase?");
         if(lowerCaseConfirm === true) {
