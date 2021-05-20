a47218f2ee5f6a63cf9fae66c499af39d23c06be
Luy10Jr <luy10jr3@gmail.com>
2021-05-20 15:22:28 -0400

    added function to Write password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +11,18 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword () {
+    var passLength = prompt("Enter how many characters you'd like. Must be 8 -128 characeters.");
+
+    var lengthConfirm = parseInt(passLength);
+    console.log(lengthConfirm);
+
+    if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+        alert("Error! Please Try Again.");
+        return;
+    }
 }
 
 // Add event listener to generate button
